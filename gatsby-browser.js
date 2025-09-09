a511833c81bc10088328a9a853545b9067341c4e
andyah <ahaguilar134@gmail.com>
2025-09-09 15:04:36 -0500

    Deploy with animations

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,14 @@ import "./src/styles/global.css"
 // gatsby-browser.js
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// gatsby-browser.js
+import AOS from "aos"
+import "aos/dist/aos.css"
+
+export const onInitialClientRender = () => {
+  AOS.init({
+    duration: 1200,
+    once: true,
+  })
+}
